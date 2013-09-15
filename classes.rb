de9cb4090d610f011aad8afc8ad3135f67d3da25
Justin Huffman <jwhuffman@gmail.com>
2013-09-15 13:53:12 -0600

    Wrote Jellybean class.

--- classes.rb
+++ classes.rb
@@ -4,6 +4,22 @@ class Dessert
         @calories = calories
     end
 
+    def name(name)
+    	@name = name
+    end
+
+    def calories(calories)
+    	@calories = calories
+    end
+
+    def name
+    	@name
+    end
+
+    def calories
+    	@calories
+    end
+
     def healthy?
         @calories < 200
     end
@@ -13,10 +29,38 @@ class Dessert
     end
 end
 
-jellybean = Dessert.new("jellybean", 30)
-puts jellybean.healthy?
-puts jellybean.delicious?
+mint = Dessert.new("mint", 30)
+puts mint.name
+puts mint.calories
+puts mint.healthy?
+puts mint.delicious?
 
 fudge = Dessert.new("fudge", 420)
+puts fudge.name
+puts fudge.calories
 puts fudge.healthy?
 puts fudge.delicious?
+
+class JellyBean < Dessert
+	def initialize(name, calories, flavor)
+		super(name, calories)
+		@flavor = flavor
+	end
+
+	def delicious?
+		return super unless @flavor == "black licorice"
+		false
+	end
+end
+
+blueberry_jb = JellyBean.new("jellybean", 36, "blueberry")
+puts blueberry_jb.name
+puts blueberry_jb.calories
+puts blueberry_jb.healthy?
+puts blueberry_jb.delicious?
+
+black_licorice_jb = JellyBean.new("jellybean", 23, "black licorice")
+puts black_licorice_jb.name
+puts black_licorice_jb.calories
+puts black_licorice_jb.healthy?
+puts black_licorice_jb.delicious?
