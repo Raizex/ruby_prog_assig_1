65b90e22c55b6f489df283e70fad72a65f7bf611
Justin Huffman <jwhuffman@gmail.com>
2013-09-15 20:41:41 -0600

    Finished CartesianProduct class.

--- cartesian_product.rb
+++ cartesian_product.rb
@@ -1,4 +1,28 @@
 class CartesianProduct
-    include Enumerable
-    # Your code here
+	include Enumerable
+	attr_accessor :a, :b, :product
+    
+    def initialize(a, b)
+    	self.a = a
+    	self.b = b
+    	calculate_product
+    end
+
+    def calculate_product
+    	self.product = []
+    	a.each do |a|
+    		b.each do |b|
+    			self.product << [a, b]
+    		end
+    	end
+    end
+
+    def each
+    	product.each do |product|
+    		yield product
+    	end
+    end
 end
+
+c = CartesianProduct.new([:a, :b], [4, 5])
+c.each {|elt| puts elt.inspect}
