430ba106e6e92f1d7f9d4aed39e9faba6482c564
Justin Huffman <jwhuffman@gmail.com>
2013-09-18 12:44:05 -0600

    Added support for dollars.

--- currency_conversion.rb
+++ currency_conversion.rb
@@ -1,5 +1,5 @@
 class Numeric
- @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
+ @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019, 'dollar' => 1}
  def method_missing(method_id)
    singular_currency = method_id.to_s.gsub( /s$/, '')
    if @@currencies.has_key?(singular_currency)
@@ -9,3 +9,7 @@ class Numeric
    end
  end
 end
+
+puts 12.yen
+puts 12.dollars
+puts 10.euros
