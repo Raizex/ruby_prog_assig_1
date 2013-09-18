56f01866b05ab51e7fb564d34130aa53d322ca8c
Justin Huffman <jwhuffman@gmail.com>
2013-09-18 13:04:30 -0600

    Finished currency conversion.

--- currency_conversion.rb
+++ currency_conversion.rb
@@ -1,15 +1,32 @@
+# Part a)
+class NoSuchCurrencyError < StandardError ; end
+
 class Numeric
- @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019, 'dollar' => 1}
- def method_missing(method_id)
-   singular_currency = method_id.to_s.gsub( /s$/, '')
-   if @@currencies.has_key?(singular_currency)
-     self * @@currencies[singular_currency]
-   else
-     super
-   end
- end
+	@@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019, 'dollar' => 1}
+	def in(currency)
+ 		singular_currency = currency.to_s.gsub( /s$/, '')
+
+ 		raise NoSuchCurrencyError unless @@currencies.has_key?(singular_currency)
+		
+		self * @@currencies[singular_currency]
+	end
+
+	def method_missing(method_id)
+		singular_currency = method_id.to_s.gsub( /s$/, '')
+		if @@currencies.has_key?(singular_currency)
+			self * @@currencies[singular_currency]
+		else
+     		super
+   		end
+ 	end
 end
 
 puts 12.yen
 puts 12.dollars
 puts 10.euros
+puts
+
+puts 1.dollars.in(:dollars)
+puts 1.yen.in(:dollars)
+# puts 1.yen.in(:japan)
+
